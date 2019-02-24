c884fe4383da742bef5c03de0a4dccccf4afafbb
zlayaa <viktoriazlayaa@gmail.com>
2019-02-24 12:21:58 +0200

    add registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users,
               path: '',
               path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
-              controllers: {omniauth_callbacks: 'omniauth_callbacks'}
+              controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
               root 'pages#index'
 
   resources :users, only: [:show]
